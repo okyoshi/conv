ae26b84b705bdaa20da5a7ad3c1fea231603d56c
okyoshi <65238629+okyoshi@users.noreply.github.com>
2023-02-11 20:09:26 +0100

    Update conv.js

--- conv.js
+++ conv.js
@@ -4,7 +4,6 @@ var from = document.getElementById("from");
 var convert = document.getElementById("convert");
 var result = document.getElementById("result");
 
-
 // Define the conversion factors for each unit
 var factors = {
   m: 1,
@@ -14,7 +13,17 @@ var factors = {
   in: 0.024,
   ft: 0.025,
   yd: 0.023,
-  mi: 0.022
+  mi: 0.022,
+  m4: 0.02115,
+  m5: 0.0204,
+  m6: 0.0196,
+  m7: 0.0187,
+  m8: 0.0183,
+  d1: 0.6956,
+  d2: 0.6956,
+  d3: 0.6956,
+  d4: 0.6956,
+  d5: 0.6956,
 };
 
 // Define the unit symbols for each unit
@@ -26,7 +35,12 @@ var symbols = {
   in: "USgph",
   ft: "USgph",
   yd: "USgph",
-  mi: "USgph"
+  mi: "USgph",
+  m4: "USgph",
+  m5: "USgph",
+  m6: "USgph",
+  m7: "USgph",
+  m8: "USgph"
 };
 
 // Add an event listener to the convert button
@@ -56,7 +70,7 @@ var outputValue = inputValue * factor / factors[inputUnit];
 // Round the output value to two decimal places
 outputValue = outputValue.toFixed(2);
 // Check if the current unit is in
-if (unit === "in" || unit === "ft" || unit === "yd" || unit === "mi" ) {
+if (unit === "in" || unit === "ft" || unit === "yd" || unit === "mi" || unit === "m4" || unit === "m5" || unit === "m6" || unit === "m7" || unit === "m8") {
   // Define the fixed numbers and the allowed difference
   var fixedNumbers = [0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.75, 0.85, 1.0, 1.11, 1.2, 1.25, 1.35, 1.5, 1.65, 1.75, 2.0, 2.25, 2.5, 2.75, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0, 7.0, 7.5, 8.0, 8.3, 9, 9.5, 10, 10.5, 11, 12, 13.8, 15.3, 17.5, 19.5, 21.5, 24, 28, 30];
   var allowedDifference = 0.01;
